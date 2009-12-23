a1150761b3807bfb44922a8b428bebe2a896eb16
Tony Angerilli <tony@angerilli.ca>
2009-12-22 21:52:41 -0800

    Fixed some regular expressions so that jabber conference rooms would match

--- django_apps/logviewer/urls.py
+++ django_apps/logviewer/urls.py
@@ -3,10 +3,10 @@ import views
 
 urlpatterns = patterns('',
     url(r'^messages/$', views.account_list, name='account_list'),
-    url(r'^messages/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s]+)/add/$', views.message_add, name='message_add'),
+    url(r'^messages/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s\-\/]+)/add/$', views.message_add, name='message_add'),
     url(r'^messages/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s]+)/$', views.other_account_list, name='other_account_list'),
-    url(r'^messages/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s\-]+)/(?P<other_uid>[\w\d\@\.\s\-]+)/$', views.message_list, name='message_list'),
+    url(r'^messages/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s\-\/]+)/(?P<other_uid>[\w\d\@\.\s\-\/]+)/$', views.message_list, name='message_list'),
     url(r'^search/$', views.search, name='search'),
-    url(r'^search/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s\-]+)/$', views.search, name='search'),
-    url(r'^search/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s\-]+)/(?P<other_uid>[\w\d\@\.\s\-]+)/$', views.search, name='search'),
+    url(r'^search/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s\-\/]+)/$', views.search, name='search'),
+    url(r'^search/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s\-\/]+)/(?P<other_uid>[\w\d\@\.\s\-]+)/$', views.search, name='search'),
 )
