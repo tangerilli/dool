afd5bdeac1f91e60cf2efab51278f0ba25d2d84e
Tony Angerilli <tony@angerilli.ca>
2009-12-22 22:05:52 -0800

    Fixed a search regex problem.
    When searching in a specific conversation list, the slash separating the two uid's was being included in the regex for the first uid.  This change means that you can't search in a jabber conference uid, but I don't think someone would ever be the "owner" of one of those anyways.

--- django_apps/logviewer/urls.py
+++ django_apps/logviewer/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     url(r'^messages/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s]+)/$', views.other_account_list, name='other_account_list'),
     url(r'^messages/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s\-\/]+)/(?P<other_uid>[\w\d\@\.\s\-\/]+)/$', views.message_list, name='message_list'),
     url(r'^search/$', views.search, name='search'),
-    url(r'^search/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s\-\/]+)/$', views.search, name='search'),
-    url(r'^search/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s\-\/]+)/(?P<other_uid>[\w\d\@\.\s\-]+)/$', views.search, name='search'),
+    url(r'^search/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s\-]+)/$', views.search, name='search'),
+    url(r'^search/(?P<protocol>\w+)/(?P<uid>[\w\d\@\.\s\-\/]+)/(?P<other_uid>[\w\d\@\.\s\-\/]+)/$', views.search, name='search'),
 )
